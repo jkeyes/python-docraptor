335f496e88c27b846360ae6e5ba207ac102c2261
John Keyes <johnkeyes@gmail.com>
2011-11-29 02:55:17 +0000

    Mirror the behaviour of the Ruby gem (not quite but close-enough).

--- docraptor/__init__.py
+++ docraptor/__init__.py
@@ -4,49 +4,156 @@
 """
 Simple wrapper for Doc Raptor API.
 """
+import json
+import os
+import re
 import requests
+from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
 # endpoint URLs
-HTTP_URL = 'http://docraptor.com/docs'
-HTTPS_URL = 'https://docraptor.com/docs'
+HTTP_URL = 'http://docraptor.com/'
+HTTPS_URL = 'https://docraptor.com/'
 
-class DocRaptorError(Exception):
-    """
-    Raised whenever there is a document generation error.
-    """
+class NoApiKeyProvidedError(RuntimeError):
+    pass
+    
+class NoContentError(KeyError):
     pass
 
-def pdf(api_key, html, params, output_file=None, ssl=False):
-    """
-    Generate a PDF document.
-    """
-    params['doc[document_type]'] = 'pdf'
-    return doc(api_key, html, params, output_file, ssl)
-    
-def xls(api_key, html, params, output_file=None, ssl=False):
-    """
-    Generate an XLS document.
-    """
-    params['doc[document_type]'] = 'xls'
-    return doc(api_key, html, params, output_file, ssl)
-    
-def doc(api_key, html, params, output_file=None, ssl=False):
-    """
-    If you want the output written to a file other than 
-    the name you supply as a param, pass an output_file 
-    (fileobj). To use the SSL endpoint, pass ssl=True.
-    """
-    params['user_credentials'] = api_key
-    params['doc[document_content]'] = html
-    url = HTTPS_URL if ssl else HTTP_URL
-    response = requests.post(url, data=params)
+class DocRaptorRequestException(StandardError):
+    def __init__(self, message, status_code):
+        super(DocRaptorRequestException, self).__init__(message)
+        self.status_code = status_code
 
-    # errors, for now dump all of them into a single message
-    if 'application/xml' in response.headers['content-type']:
-        raise DocRaptorError(response.content)
+    def __str__(self):
+        return "%s\nHTTP Status: %s\nReturned: %s" % (self.__class__.__name__, self.status_code, self.message)
 
-    if output_file is None:
-        output_file = open(params['doc[name]'], 'w')
-    output_file.write(response.content)
-    return response.content
+class DocumentCreationFailure(DocRaptorRequestException):
+    pass
+
+class DocumentListingFailure(DocRaptorRequestException):
+    pass
+
+class DocumentStatusFailure(DocRaptorRequestException):
+    pass
+
+class DocumentDownloadFailure(DocRaptorRequestException):
+    pass
+
+class DocRaptor(object):
+
+    def __init__(self, api_key=None):
+        self.api_key = api_key if api_key is not None else os.environ.get('DOCRAPTOR_API_KEY', None)
+        if not self.api_key:
+            raise NoApiKeyProvidedError("No API key provided")
+
+    def create(self, options=None):
+        if options is None:
+            raise ValueError("Please pass in an options dict")
+        if not (bool(options.get('document_content', False) or options.get('document_url', False))):
+
+            raise NoContentError("must supply 'document_content' or 'document_url'")
+
+        default_options = {
+            'name': 'default',
+            'document_type': 'pdf',
+            'test': False,
+            'async': False,
+            'raise_exception_on_failure': False
+        }
+        options = dict(default_options.items() + options.items())
+        raise_exception_on_failure = options.pop('raise_exception_on_failure')
+        query = { 'user_credentials': self.api_key }
+        if options['async']:
+            query['output'] = 'json'
+        doc_options = _format_keys({'doc': options})
+        
+        url = os.environ.get("DOCRAPTOR_URL", HTTPS_URL)
+        resp = requests.post('%sdocs' % (url), data=doc_options, params=query)
+
+        if raise_exception_on_failure and resp.status_code != 200:
+            raise DocumentCreationFailure(resp.content, resp.status_code)
+            
+        if options['async']:
+            as_json = json.loads(resp.content)
+            self.status_id = as_json['status_id']
+        else:
+            return resp
+        
+    def list_docs(self, options=None):
+        if options is None:
+            raise ValueError("Please pass in an options dict")
+
+        default_options = { 
+            'page': 1,
+            'per_page': 100,
+            'raise_exception_on_failure': False,
+            'user_credentials': self.api_key
+        }
+        options = dict(default_options.items() + options.items())
+        raise_exception_on_failure = options.pop('raise_exception_on_failure')
+        url = os.environ.get("DOCRAPTOR_URL", HTTPS_URL)
+        resp = requests.get('%sdocs' % (url), params=options)
+        
+        if raise_exception_on_failure and resp.status_code != 200:
+            raise DocumentListingFailure(resp.content, resp.status_code)
+        return resp
+        
+    def status(self, status_id, raise_exception_on_failure=False):
+        query = { 
+            'output': 'json',
+            'user_credentials': self.api_key 
+        }
+        url = os.environ.get("DOCRAPTOR_URL", HTTPS_URL)
+        resp = requests.get('%sstatus/#%s' % (url, status_id), params=query)
+        if raise_exception_on_failure and resp.status_code != 200:
+            raise DocumentStatusFailure(resp.content, resp.status_code)
+
+        if resp.status_code == 200:
+            as_json = json.loads(resp.content) #'{ "status": "completed", "download_url": "/blah/download/jkskskdldkd/" }') #resp.content)
+            if as_json['status'] == 'completed':
+                as_json['download_key'] = re.match('/.*?\/download\/(.+)/', as_json['download_url']).groups()[0]
+            return as_json
+        return resp
+        
+    def download(self, download_key, raise_exception_on_failure=False):
+        query = { 
+            'output': 'json',
+            'user_credentials': self.api_key 
+        }
+        url = os.environ.get("DOCRAPTOR_URL", HTTPS_URL)
+        resp = requests.get('%s/download/%s' % (url, download_key), params=query)
+        if raise_exception_on_failure and resp.status_code != 200:
+            raise DocumentDownloadFailure(resp.content, resp.status_code)
+        return resp
+        
+def _format_keys(options, result=None, parent_key=None):
+    if result is None:
+        result = {}
+    for k, v in options.items():
+        if parent_key:
+            key = '%s[%s]' % (parent_key, k)
+        else:
+            key= k
+        if isinstance(v, dict):
+            _format_keys(v, result, key)
+        elif v is False: result[key] = 'false'
+        elif v is True: result[key] = 'true'
+        else:
+            result[key] = v
+    return result
+
+if __name__ == "__main__":
+    docraptor = DocRaptor('Ba7ZA84mA9Ejmhrpde8a')
+    with open("jk_new.pdf", "wb") as f:
+        f.write(docraptor.create({ 'document_content': '<p>Test</p>', 'test': True, 'prince_options': { 'disallow_print': True }, 'raise_exception_on_failure': True }))
+
+    resp = docraptor.create({ 'document_content': '<p>Test</p>', 'test': True, 'prince_options': { 'disallow_print': True }, 'async': True })
+    try:
+        resp = docraptor.status(docraptor.status_id, True)
+        print resp
+    except Exception, e:
+        print e
+    resp = docraptor.list_docs({})
+    
---
+++ test/tests.py
@@ -0,0 +1,181 @@
+# Copyright (C) 2011 John Keyes
+# http://jkeyes.mit-license.org/
+
+import os
+from os import path
+from docraptor import DocRaptor
+from docraptor import DocumentCreationFailure
+from docraptor import DocumentStatusFailure
+from docraptor import DocumentListingFailure
+from docraptor import DocumentDownloadFailure
+from docraptor import NoApiKeyProvidedError
+from docraptor import NoContentError
+import requests
+from unittest import TestCase
+from nose.tools import raises
+
+TEST = "test.pdf"
+TEST_NAME = "test_name.pdf"
+FILES = [TEST, TEST_NAME]
+DIRPATH = os.path.dirname(__file__)
+FIXTURES = {}
+
+def _clean():
+    for fname in FILES:
+        if path.exists(fname):
+            os.unlink(fname)
+
+class MockResponse(object):
+    pass
+
+def stub_http_response_with(filename, method=None, status=None):
+    if status is None:
+        status = 200
+    content = open(os.path.join(DIRPATH, 'fixtures', filename), "r").read()
+    FIXTURES[filename] = content
+    def stubbed(*args, **kwargs):
+        resp = MockResponse()
+        resp.status_code = status
+        resp.content = content
+        return resp
+    setattr(requests, method, stubbed)
+
+class DocRaptorApiKeyTest(TestCase):
+
+    def setUp(self):
+        os.environ.pop('DOCRAPTOR_API_KEY', None)
+        self.test_key = "test key"
+
+    @raises(NoApiKeyProvidedError)
+    def test_api_keys(self):
+        docraptor = DocRaptor()
+
+    def test_env_api_key(self):
+        os.environ['DOCRAPTOR_API_KEY'] = self.test_key
+        docraptor = DocRaptor()
+        assert docraptor.api_key == self.test_key
+
+    def test_no_overwrite_env_api_key(self):
+        os.environ['DOCRAPTOR_API_KEY'] = self.test_key
+        docraptor = DocRaptor()
+
+        os.environ['DOCRAPTOR_API_KEY'] = "blah"
+        assert docraptor.api_key == self.test_key
+
+    def test_param_api_keys(self):
+        docraptor = DocRaptor(self.test_key)
+        assert docraptor.api_key == self.test_key
+
+class DocRaptorCreateTest(TestCase):
+    def setUp(self):
+        self.test_key = "test key"
+
+    @raises(AttributeError)
+    def test_bogus_arguments_bool(self):
+        DocRaptor(self.test_key).create(True)
+
+    @raises(ValueError)
+    def test_bogus_arguments_none(self):
+        DocRaptor(self.test_key).create(None)
+
+    @raises(NoContentError)
+    def test_no_content_empty(self):
+        DocRaptor(self.test_key).create({})
+
+    @raises(NoContentError)
+    def test_no_content_attr(self):
+        DocRaptor(self.test_key).create({'herped': 'the_derp'})
+
+    @raises(NoContentError)
+    def test_blank_content(self):
+        DocRaptor(self.test_key).create({ 'content': ''})
+
+    @raises(NoContentError)
+    def test_blank_url(self):
+        DocRaptor(self.test_key).create({'url': ''})
+    
+class DocRaptorDocumentContentTest(TestCase):
+    def setUp(self):
+        self.test_key = "test key"
+        self.html_content = "<html><body>Hey</body></html>"
+
+    def test_create_with_invalid_content(self):
+        invalid_html = "<herp"
+        stub_http_response_with("invalid_pdf", "post", 422)
+        resp = DocRaptor(self.test_key).create({'document_content': invalid_html })
+        assert FIXTURES["invalid_pdf"] == resp.content
+        assert 422 == resp.status_code
+
+    @raises(DocumentCreationFailure)
+    def test_create_with_invalid_content_raises(self):
+        invalid_html = "<herp"
+        stub_http_response_with("invalid_pdf", "post", 422)
+        resp = DocRaptor(self.test_key).create({'document_content': invalid_html, 'raise_exception_on_failure': True })
+
+    def test_create_with_valid_content(self):
+        stub_http_response_with("simple_pdf", "post")
+        resp = DocRaptor(self.test_key).create({'document_content': self.html_content })
+        assert FIXTURES["simple_pdf"] == resp.content
+        assert 200 == resp.status_code
+
+class DocRaptorListDocsTest(TestCase):
+    def setUp(self):
+        self.test_key = "test key"
+
+    @raises(AttributeError)
+    def test_bogus_arguments_bool(self):
+        DocRaptor(self.test_key).list_docs(True)
+
+    @raises(ValueError)
+    def test_bogus_arguments_None(self):
+        DocRaptor(self.test_key).list_docs(None)
+
+    @raises(DocumentListingFailure)
+    def test_invalid_list_docs_raises(self):
+        stub_http_response_with("invalid_list_docs", "get", 400)
+        resp = DocRaptor(self.test_key).list_docs({'raise_exception_on_failure': True})
+
+    def test_list_docs(self):
+        stub_http_response_with("simple_list_docs", "get")
+        resp = DocRaptor(self.test_key).list_docs({})
+        assert FIXTURES["simple_list_docs"] == resp.content
+
+class DocRaptorStatusTest(TestCase):
+    def setUp(self):
+        self.test_key = "test key"
+
+    def test_invalid_status(self):
+        stub_http_response_with("invalid_status", "get", 403)
+        resp = DocRaptor(self.test_key).status("test-id")
+        assert FIXTURES["invalid_status"] == resp.content
+        assert 403 == resp.status_code
+
+    @raises(DocumentStatusFailure)
+    def test_invalid_status_raises(self):
+        stub_http_response_with("invalid_status", "get", 400)
+        resp = DocRaptor(self.test_key).status("test-id", True)
+
+    def atest_status(self):
+        stub_http_response_with("simple_status", "get")
+        resp = DocRaptor(self.test_key).status("test-id")
+        assert "completed" == resp['status']
+
+class DocRaptorDownloadTest(TestCase):
+    def setUp(self):
+        self.test_key = "test key"
+
+    def test_invalid_download(self):
+        stub_http_response_with("invalid_download", "get", 400)
+        resp = DocRaptor(self.test_key).download("test-id")
+        assert FIXTURES["invalid_download"] == resp.content
+        assert 400 == resp.status_code
+
+    @raises(DocumentDownloadFailure)
+    def test_invalid_download_raises(self):
+        stub_http_response_with("invalid_download", "get", 400)
+        resp = DocRaptor(self.test_key).download("test-id", True)
+        
+    def test_download(self):
+        stub_http_response_with("simple_download", "get")
+        resp = DocRaptor(self.test_key).download("test-id")
+        assert FIXTURES["simple_download"] == resp.content
