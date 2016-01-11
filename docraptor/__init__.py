6cbf9c174a6a9813de23d6d32705780bbf931b69
John Keyes <john@keyes.ie>
2016-01-11 12:02:52 +0000

    PEP8ing and Flake8ing.

--- docraptor/__init__.py
+++ docraptor/__init__.py
@@ -8,23 +8,25 @@ import json
 import os
 import re
 import requests
-from requests.auth import HTTPBasicAuth
-from datetime import datetime
+
 
 ENV = os.environ
 API_KEY = "DOCRAPTOR_API_KEY"
 URL = "DOCRAPTOR_URL"
-DEFAULT_TIMEOUT = 90 #seconds
+DEFAULT_TIMEOUT = 90  # seconds
 TIMEOUT = "DOCRAPTOR_TIMEOUT"
 
 # endpoint URLs
 HTTPS_URL = 'https://docraptor.com/'
 
+
 class NoApiKeyProvidedError(RuntimeError):
     pass
-    
+
+
 class NoContentError(KeyError):
     pass
+
 
 class DocRaptorRequestException(Exception):
     def __init__(self, message, status_code):
@@ -32,19 +34,25 @@ class DocRaptorRequestException(Exception):
         self.status_code = status_code
 
     def __str__(self):
-        return "%s\nHTTP Status: %s\nReturned: %s" % (self.__class__.__name__, self.status_code, self.message)
+        return "%s\nHTTP Status: %s\nReturned: %s" % (
+            self.__class__.__name__, self.status_code, self.message)
+
 
 class DocumentCreationFailure(DocRaptorRequestException):
     pass
 
+
 class DocumentListingFailure(DocRaptorRequestException):
     pass
+
 
 class DocumentStatusFailure(DocRaptorRequestException):
     pass
 
+
 class DocumentDownloadFailure(DocRaptorRequestException):
     pass
+
 
 class DocRaptor(object):
 
@@ -58,9 +66,10 @@ class DocRaptor(object):
     def create(self, options=None):
         if options is None:
             raise ValueError("Please pass in an options dict")
-        
+
         if not _has_content(options):
-            raise NoContentError("must supply 'document_content' or 'document_url'")
+            raise NoContentError(
+                "must supply 'document_content' or 'document_url'")
 
         default_options = {
             'name': 'default',
@@ -71,26 +80,28 @@ class DocRaptor(object):
         }
         options = dict(list(default_options.items()) + list(options.items()))
         raise_exception_on_failure = options.pop('raise_exception_on_failure')
-        query = { 'user_credentials': self.api_key }
+        query = {'user_credentials': self.api_key}
         if options['async']:
             query['output'] = 'json'
         doc_options = _format_keys({'doc': options})
-        
-        resp = requests.post('%sdocs' % (self._url), data=doc_options, params=query, timeout=self._timeout)
+
+        resp = requests.post(
+            '%sdocs' % (self._url), data=doc_options, params=query,
+            timeout=self._timeout)
 
         if raise_exception_on_failure and resp.status_code != 200:
             raise DocumentCreationFailure(resp.content, resp.status_code)
-            
+
         if options['async']:
             return json.loads(resp.content.decode("utf-8"))
         else:
             return resp
-        
+
     def list_docs(self, options=None):
         if options is None:
             raise ValueError("Please pass in an options dict")
 
-        default_options = { 
+        default_options = {
             'page': 1,
             'per_page': 100,
             'raise_exception_on_failure': False,
@@ -99,19 +110,22 @@ class DocRaptor(object):
         options = dict(list(default_options.items()) + list(options.items()))
         raise_exception_on_failure = options.pop('raise_exception_on_failure')
 
-        resp = requests.get('%sdocs' % (self._url), params=options, timeout=self._timeout)
-        
+        resp = requests.get(
+            '%sdocs' % (self._url), params=options, timeout=self._timeout)
+
         if raise_exception_on_failure and resp.status_code != 200:
             raise DocumentListingFailure(resp.content, resp.status_code)
         return resp
-        
+
     def status(self, status_id, raise_exception_on_failure=False):
-        query = { 
+        query = {
             'output': 'json',
-            'user_credentials': self.api_key 
+            'user_credentials': self.api_key
         }
 
-        resp = requests.get('%sstatus/%s' % (self._url, status_id), params=query, timeout=self._timeout)
+        resp = requests.get(
+            '%sstatus/%s' % (self._url, status_id), params=query,
+            timeout=self._timeout)
 
         if raise_exception_on_failure and resp.status_code != 200:
             raise DocumentStatusFailure(resp.content, resp.status_code)
@@ -119,24 +133,30 @@ class DocRaptor(object):
         if resp.status_code == 200:
             as_json = json.loads(resp.content.decode("utf-8"))
             if as_json['status'] == 'completed':
-                as_json['download_key'] = re.match('.*?\/download\/(.+)', as_json['download_url']).groups()[0]
+                match = re.match(
+                    '.*?\/download\/(.+)', as_json['download_url'])
+                as_json['download_key'] = match.groups()[0]
             return as_json
         return resp
-        
+
     def download(self, download_key, raise_exception_on_failure=False):
-        query = { 
+        query = {
             'output': 'json',
-            'user_credentials': self.api_key 
+            'user_credentials': self.api_key
         }
-        resp = requests.get('%sdownload/%s' % (self._url, download_key), params=query, timeout=self._timeout)
+        resp = requests.get(
+            '%sdownload/%s' % (self._url, download_key), params=query,
+            timeout=self._timeout)
 
         if raise_exception_on_failure and resp.status_code != 200:
             raise DocumentDownloadFailure(resp.content, resp.status_code)
         return resp
 
+
 def _has_content(options):
     content = options.get('document_content') or options.get('document_url')
     return bool(content)
+
 
 def _format_keys(options, result=None, parent_key=None):
     if result is None:
@@ -145,11 +165,13 @@ def _format_keys(options, result=None, parent_key=None):
         if parent_key:
             key = '%s[%s]' % (parent_key, k)
         else:
-            key= k
+            key = k
         if isinstance(v, dict):
             _format_keys(v, result, key)
-        elif v is False: result[key] = 'false'
-        elif v is True: result[key] = 'true'
+        elif v is False:
+            result[key] = 'false'
+        elif v is True:
+            result[key] = 'true'
         else:
             result[key] = v
     return result
