7b204c7da931c276cd938f45c0cee987bcd55320
John Keyes <johnkeyes@tapadoo.com>
2019-03-08 12:22:41 +0000

    Adding __main__ scope protection to examples.

--- example/basic_url.py
+++ example/basic_url.py
@@ -1,9 +1,11 @@
 from docraptor import DocRaptor
 
-docraptor = DocRaptor()
+if __name__ == "__main__":
+    print(">>><<<" * 20)
+    docraptor = DocRaptor()
 
-print("Create test_basic_url.pdf")
-with open("test_basic_url.pdf", "wb") as f:
-    f.write(
-        docraptor.create({"document_url": "http://docraptor.com", "test": True}).content
-    )
+    print("Create test_basic_url.pdf")
+    with open("test_basic_url.pdf", "wb") as f:
+        f.write(
+            docraptor.create({"document_url": "http://docraptor.com", "test": True}).content
+        )
--- example/basic_xls.py
+++ example/basic_xls.py
@@ -1,21 +1,22 @@
 from docraptor import DocRaptor
 
-table = """<table>
-<thead>
-<tr><th>First Name</th><th>Last Name</th></tr>
-</thead>
-<tbody>
-<tr><td>Paul</td><td>McGrath</td></tr>
-<tr><td>Liam</td><td>Brady</td></tr>
-<tr><td>John</td><td>Giles</td></tr>
-</tbody>
-</table>"""
-docraptor = DocRaptor()
+if __name__ == "__main__":
+    table = """<table>
+    <thead>
+    <tr><th>First Name</th><th>Last Name</th></tr>
+    </thead>
+    <tbody>
+    <tr><td>Paul</td><td>McGrath</td></tr>
+    <tr><td>Liam</td><td>Brady</td></tr>
+    <tr><td>John</td><td>Giles</td></tr>
+    </tbody>
+    </table>"""
+    docraptor = DocRaptor()
 
-print("Create test_basic.xls")
-with open("test_basic.xls", "wb") as f:
-    f.write(
-        docraptor.create(
-            {"document_content": table, "document_type": "xls", "test": True}
-        ).content
-    )
+    print("Create test_basic.xls")
+    with open("test_basic.xls", "wb") as f:
+        f.write(
+            docraptor.create(
+                {"document_content": table, "document_type": "xls", "test": True}
+            ).content
+        )
