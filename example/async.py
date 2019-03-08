33e39db120566c3401e86621f117d29944cb1a5e
John Keyes <johnkeyes@tapadoo.com>
2019-03-08 09:00:56 +0000

    Fixing formatting errors in example code and updating prints.

--- example/async.py
+++ example/async.py
@@ -3,24 +3,24 @@ from docraptor import DocRaptor
 
 docraptor = DocRaptor()
 
-print "Create PDF"
+print("Create PDF")
 resp = docraptor.create({
-    'document_content': '<h1>python-docraptor</h1><p>Async Test</p>', 
-    'test': True, 
-    'async': True 
+    'document_content': '<h1>python-docraptor</h1><p>Async Test</p>',
+    'test': True,
+    'async': True
 })
-print "Status ID: %s" % (resp['status_id'])
+print(f"Status ID: {resp['status_id']}")
 
 status_id = resp['status_id']
 resp = docraptor.status(status_id)
 
-print "    %s" % (resp['status']) 
+print(f"    {resp['status']}")
 while resp['status'] != 'completed':
     time.sleep(3)
     resp = docraptor.status(status_id)
-    print "    %s" % (resp['status']) 
-    
-print "Download to test_async.pdf",
+    print(f"    {resp['status']}")
+
+print("Download to test_async.pdf")
 with open("test_async.pdf", "wb") as f:
     f.write(docraptor.download(resp['download_key']).content)
-print "[DONE]"
+print("[DONE]")
